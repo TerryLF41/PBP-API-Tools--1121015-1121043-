bfe8790cc4a279d09b004775527b42657ccdc063
TerryLF <if-21015@students.ithb.ac.id>
2023-03-30 01:42:30 +0700

    template goCron+goMail

--- controller/controller.go
+++ controller/controller.go
@@ -1,1 +1,35 @@
 package controller
+
+import (
+	"time"
+
+	"github.com/go-co-op/gocron"
+	"gopkg.in/gomail.v2"
+)
+
+// GoCron
+func runScheduler() {
+	s := gocron.NewScheduler(time.UTC)
+
+	s.Every(1).Days().Do(func() {
+		//Func Kirim Email
+	})
+
+	s.StartBlocking()
+}
+
+// GoMail
+func sendMail() {
+	msg := gomail.NewMessage()
+	msg.SetHeader("From", "EMAIL ADMIN")
+	msg.SetHeader("To", "EMAIL USER")
+	msg.SetHeader("Subject", "TITLE BERITA")
+	msg.SetBody("text/html", "<p>ISI BERITA</p>")
+
+	n := gomail.NewDialer("smtp.gmail.com", 587, "EMAIL ADMIN", "PASSWORD EMAIL ADMIN")
+
+	// Send the email
+	if err := n.DialAndSend(msg); err != nil {
+		panic(err)
+	}
+}
