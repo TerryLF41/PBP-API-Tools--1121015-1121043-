f5410591fce9bb6085180a988d52ba49b3d038a3
TerryLF <if-21015@students.ithb.ac.id>
2023-03-30 01:51:02 +0700

    Update model.go

--- controller/model.go
+++ controller/model.go
@@ -1,1 +1,15 @@
 package controller
+
+type User struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Email    string `json:"email"`
+}
+
+type Berita struct {
+	ID      int    `json:"id"`
+	Tanggal string `json:"tanggal"`
+	Title   string `json:"title"`
+	Isi     string `json:"isi"`
+}
